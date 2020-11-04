2b4a09d378875a67e2434baa18e44ea030cfa113
Twen Z <t.wen.z.235@gmail.com>
2020-11-04 13:26:43 +0700

    add yarn test local run

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from 'antd'
 
 const App = () => {
-  return <div>react App</div>;
-};
+  return (
+    <div>
+      <div>react App</div>
+      <Button>Button</Button>
+    </div>
+  )
+}
 
 export default App;
